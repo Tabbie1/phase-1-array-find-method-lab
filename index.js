27148eb1173b36a3e822ed38046f1ff8a0faf063
tabitha muriithi <excellentwriter120@gmail.com>
2024-01-08 11:03:52 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(records){
+    2015 ;"W",
+    2014 ; "N/A",
+    2013 ; "L"
+    const winningYear = records.find(record => record.result ==="W");
+    return winningYear ? winningYear.year : undefined;
+    
+}
